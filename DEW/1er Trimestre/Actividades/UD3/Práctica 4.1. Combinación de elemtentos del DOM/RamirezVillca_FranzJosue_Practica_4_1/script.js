9f9ea0acb662eb55aa28b9a0ec0674b883d6e830
josue <josue0dev@gmail.com>
2018-11-06 21:22:41 +0000

    DOM

--- DEW/1er Trimestre/Actividades/UD3/Práctica 4.1. Combinación de elemtentos del DOM/RamirezVillca_FranzJosue_Practica_4_1/script.js
+++ DEW/1er Trimestre/Actividades/UD3/Práctica 4.1. Combinación de elemtentos del DOM/RamirezVillca_FranzJosue_Practica_4_1/script.js
@@ -1,29 +1,97 @@
+let colorAmarillo = "yellow", colorVerde = "green", colorRojo = "red",
+  fuenteFantasy = "fantasy", fuenteMonospace = "monospace", fuenteSansSerif = "sans-serif",
+  textoNegrita = "bold", decoracionTexto = "underline", estiloFuenteItalic = "italic";
+
 let columna = document.getElementById("columna");
 let fila = document.getElementById("fila");
 let tabla = document.getElementsByTagName("table").item(0);
+let formulario = document.getElementsByTagName("form").item(0);
 
-let nodeTR = document.createElement("tr");
-let nodeTD = document.createElement("td");
-nodeTD.setAttribute("onclick", "ShowTextFormated(this)");
-let nodeTDText = document.createTextNode("");
-nodeTD.appendChild(nodeTDText);
+if (GetCookie("tamanoTabla") != null) {
+  document.getElementById("formularioTabla").remove();
+  let tamanoTabla = GetCookie("tamanoTabla");
+  tamanoTabla = tamanoTabla.split(":");
+  columna.value = tamanoTabla[0];
+  fila.value = tamanoTabla[1];
+  CrearTabla();
+}
 
-document.getElementById("genTabla").addEventListener("click", function () {
+function CrearTabla() {
+  let nodeTR = document.createElement("tr");
+  let nodeTD = document.createElement("td");
+  nodeTD.setAttribute("onclick", "MostrarTexto(this)");
+  let nodeTDText = document.createTextNode("");
+  nodeTD.appendChild(nodeTDText);
+
+  //Crea una fila con todas las columnas
   for (let i = 0; i < parseInt(columna.value); i++) {
-    tabla.appendChild(nodeTR);
     nodeTR.appendChild(nodeTD.cloneNode(true));
   }
-  for (let i = 0; i < parseInt(fila.value) - 1; i++) {
+  //Rellena las filas restantes 
+  for (let i = 0; i < parseInt(fila.value); i++) {
     tabla.appendChild(nodeTR.cloneNode(true));
   }
-});
-
-function ShowTextFormated(e) {
-  var data = new FormData(document.getElementsByTagName("form").item(0));
-  var output = "";
-  for (const entry of data) {
-    output = entry[0] + "=" + entry[1] + "\r";
-  };
-  e.innerText = output;
-  event.preventDefault()
+  
+  if (GetCookie("tamanoTabla") === null ) {
+    SetCookie("tamanoTabla", columna.value + ":" + fila.value, 3);    
+  }
+  document.getElementById("formularioTabla").remove();
 }
+
+function MostrarTexto(e) {
+  let datos = new FormData(formulario);
+  e.innerText = "X";
+  for (const item of datos) {
+    switch (item[1].toString()) {
+      case colorAmarillo:
+        e.style.color = colorAmarillo;
+        break;
+      case colorVerde:
+        e.style.color = colorVerde;
+        break;
+      case colorRojo:
+        e.style.color = colorRojo;
+        break;
+      case fuenteFantasy:
+        e.style.fontFamily = fuenteFantasy;
+        break;
+      case fuenteMonospace:
+        e.style.fontFamily = fuenteMonospace;
+        break;
+      case fuenteSansSerif:
+        e.style.fontFamily = fuenteSansSerif;
+        break;
+      case textoNegrita:
+        e.style.fontWeight = textoNegrita;
+        break;
+      case estiloFuenteItalic:
+        e.style.fontStyle = estiloFuenteItalic;
+        break;
+      case decoracionTexto:
+        e.style.textDecoration = decoracionTexto;
+        break;
+    }
+  };
+}
+
+function GetCookie(name) {
+  let index = document.cookie.indexOf(name + "=");
+  if (index == -1) {
+    return null;
+  }
+  index = document.cookie.indexOf("=", index) + 1;
+  let endStr = document.cookie.indexOf(";", index);
+  if (endStr == -1) {
+    endStr = document.cookie.length;
+  }
+  return decodeURIComponent(document.cookie.substring(index, endStr));
+}
+
+function SetCookie(name, value, exdays) {
+  let date = new Date();
+  date.setTime(date.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires" + "=" + date.toUTCString();
+  document.cookie = name + "=" + encodeURIComponent(value) + ";" + expires + ";path=" + window.location.href + ";";
+}
+
+document.getElementById("genTabla").addEventListener("click", CrearTabla);
