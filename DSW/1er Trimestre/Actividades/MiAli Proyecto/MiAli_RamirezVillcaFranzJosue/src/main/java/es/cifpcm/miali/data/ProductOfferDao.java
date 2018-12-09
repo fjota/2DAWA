bd6a3c6f68aad000542ca4da746c538ffdb803d8
josue <josue0dev@gmail.com>
2018-12-09 17:20:14 +0000

    dsw

---
+++ DSW/1er Trimestre/Actividades/MiAli Proyecto/MiAli_RamirezVillcaFranzJosue/src/main/java/es/cifpcm/miali/data/ProductOfferDao.java
@@ -0,0 +1,13 @@
+package es.cifpcm.miali.data;
+
+import es.cifpcm.miali.model.product.ProductOffer;
+
+/**
+ *
+ * @author Josué Ramírez
+ */
+public interface ProductOfferDao {
+  
+  public ProductOffer selectAll();
+  
+}
---
+++ DSW/1er Trimestre/Actividades/MiAli Proyecto/MiAli_RamirezVillcaFranzJosue/src/main/java/es/cifpcm/miali/data/ProductOfferDaoImpl.java
@@ -0,0 +1,15 @@
+package es.cifpcm.miali.data;
+
+import es.cifpcm.miali.model.product.ProductOffer;
+
+/**
+ *
+ * @author Josué Ramírez
+ */
+public class ProductOfferDaoImpl implements ProductOfferDao {
+
+  @Override
+  public ProductOffer selectAll() {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }      
+}
