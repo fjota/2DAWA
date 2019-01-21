23f1a7a9e72fc0d9385522b49233eb091957461d
josue <josue0dev@gmail.com>
2019-01-21 22:26:02 +0000

    rest-dsw

--- DSW/2do_Trimestre/Actividades/AUT08_01 Introducción a JAX-RS/ActorsRs_RamirezJosue/AUT08_01_RamirezVillcaFranzJosue/src/main/java/es/cifpcm/actorsrs_ramirezjosue/repository/ActorRepository.java
+++ DSW/2do_Trimestre/Actividades/AUT08_01 Introducción a JAX-RS/ActorsRs_RamirezJosue/AUT08_01_RamirezVillcaFranzJosue/src/main/java/es/cifpcm/actorsrs_ramirezjosue/repository/ActorRepository.java
@@ -3,9 +3,12 @@ package es.cifpcm.actorsrs_ramirezjosue.repository;
 import es.cifpcm.actorsrs_ramirezjosue.data.HibernateUtil;
 import es.cifpcm.actorsrs_ramirezjosue.model.Actor;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.hibernate.Session;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -13,7 +16,7 @@ import javax.persistence.criteria.CriteriaQuery;
  */
 public class ActorRepository {
 
-  public static List<Actor> getAll() {
+  public static List<Actor> findAll() {
     List<Actor> list = new ArrayList<>();
     Session session = HibernateUtil.getSessionFactory().openSession();
 
@@ -23,7 +26,7 @@ public class ActorRepository {
       CriteriaQuery<Actor> criteriaQuery = session.getCriteriaBuilder().createQuery(Actor.class);
       criteriaQuery.from(Actor.class);
       list = session.createQuery(criteriaQuery).getResultList();
-      
+
       session.getTransaction().commit();
     } catch (Exception ex) {
       session.getTransaction().rollback();
@@ -32,6 +35,48 @@ public class ActorRepository {
     }
 
     return list;
+  }
+
+  public static Actor findOne(int id) {
+    Actor actor = new Actor();
+    Session session = HibernateUtil.getSessionFactory().openSession();
+
+    try {
+      session.beginTransaction();
+
+      CriteriaQuery<Actor> criteriaQuery = session.getCriteriaBuilder().createQuery(Actor.class);
+      Root<Actor> actorRoot = criteriaQuery.from(Actor.class);
+      criteriaQuery.where(session.getCriteriaBuilder().equal(actorRoot.get("actorId"), id));
+      actor = session.createQuery(criteriaQuery).getSingleResult();
+      
+      session.getTransaction().commit();
+    } catch (Exception ex) {
+      session.getTransaction().rollback();
+    } finally {
+      session.close();
+    }
+
+    return actor;
+  }
+  
+  public static Response registerActor(String name, String lastName){
+    Session session = HibernateUtil.getSessionFactory().openSession();
+
+    try {
+      session.beginTransaction();
+
+      Actor actor = new Actor(name, lastName, new Date());
+      session.save(actor);
+      
+      session.getTransaction().commit();
+    } catch (Exception ex) {
+      session.getTransaction().rollback();
+      Response.serverError().entity("Actor id not found").build();
+    } finally {
+      session.close();
+    }
+    
+    return Response.ok("Success").build();
   }
 
 }
--- DSW/2do_Trimestre/Actividades/AUT08_01 Introducción a JAX-RS/ActorsRs_RamirezJosue/AUT08_01_RamirezVillcaFranzJosue/src/main/java/es/cifpcm/actorsrs_ramirezjosue/services/rest/ActorRest.java
+++ DSW/2do_Trimestre/Actividades/AUT08_01 Introducción a JAX-RS/ActorsRs_RamirezJosue/AUT08_01_RamirezVillcaFranzJosue/src/main/java/es/cifpcm/actorsrs_ramirezjosue/services/rest/ActorRest.java
@@ -1,11 +1,18 @@
 package es.cifpcm.actorsrs_ramirezjosue.services.rest;
 
+import com.google.gson.Gson;
 import es.cifpcm.actorsrs_ramirezjosue.model.Actor;
 import es.cifpcm.actorsrs_ramirezjosue.repository.ActorRepository;
+import java.util.List;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 /**
@@ -14,27 +21,36 @@ import javax.ws.rs.core.UriInfo;
  */
 @Path("actors")
 public class ActorRest {
-      
+
   @Context
   private UriInfo context;
-  
-  public ActorRest(){
-    
+
+  public ActorRest() {
+
+  }
+
+  @GET
+  @Path("/")
+  @Produces(MediaType.APPLICATION_JSON)
+  public String findAll() {
+    List<Actor> listActors = ActorRepository.findAll();
+    String json = new Gson().toJson(listActors);
+    return json;
   }
   
   @GET
-  @Produces("text/html")
-  public String read(){
-    //return ActorRepository.getAll().toString();
-    return ActorRepository.getAll().toString();
-    /*ActorRepository actorRepository = new  ActorRepository();
-    StringBuilder sb = new StringBuilder();
-    
-    for (Actor element : actorRepository.getAll()) {
-      sb.append("<p>").append(element.getActorId()).append(" ").append(element.getFirstName()).append(" ").append(element.getLastName()).append(" ").append(element.getLastUpdate()).append("</p>");
-    }
-    return sb.toString();*/
-    //return "<h1>hola</h1>";
+  @Path("{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public String findOne(@PathParam("id") int id){
+    String json = new Gson().toJson(ActorRepository.findOne(id));
+    return json;
   }
   
+  @POST
+  @Path("/")
+  public Response registerActor(@QueryParam("name") String name, @QueryParam("last_name") String lastName){
+    Response res = ActorRepository.registerActor(name, lastName);
+    return res;
+  }
+
 }
