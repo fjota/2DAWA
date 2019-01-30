19a1d7080d56cccab6de0df24544234b3715a24a
franz josue <josue0dev@gmail.com>
2019-01-30 13:56:45 +0000

    dew-jquery-menu

--- DSW/2do_Trimestre/Actividades/Employees/Employees/App_Start/RouteConfig.cs
+++ DSW/2do_Trimestre/Actividades/Employees/Employees/App_Start/RouteConfig.cs
@@ -7,17 +7,25 @@ using System.Web.Routing;
 
 namespace Employees
 {
-    public class RouteConfig
+  public class RouteConfig
+  {
+    public static void RegisterRoutes(RouteCollection routes)
     {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+      routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-        }
+      routes.MapRoute(
+         name: "Segunda",
+         url: "{controller}/{action}/{id}",
+         defaults: new { controller = "Test", action = "GetCustomer", id = UrlParameter.Optional }
+      );
+
+      routes.MapRoute(
+          name: "Default",
+          url: "{controller}/{action}/{id}",
+          defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+      );
+
+      
     }
+  }
 }
