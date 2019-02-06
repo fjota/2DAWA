88d0460cac1317d3d6a41b7fee107fa633f4fda8
josue <josue0dev@gmail.com>
2019-02-06 15:27:41 +0000

    .-.

---
+++ DSW/2do/Actividades/AUT09_01/DiscosRamirezFranzJosue/DiscosRamirezFranzJosue/Models/OrderModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiscosRamirezFranzJosue.Models
+{
+  public class OrderModel
+  {
+    public int[] SortedIndex { get; }
+    public string[] SortedNames { get; }
+
+    public OrderModel()
+    {
+      SortedIndex = new int[]{ 0,1,2,3};
+      SortedNames = new string[] {"Orden por defecto", "Alfabéticamanete ascendente (A-Z)",
+      "Alfabéticamanete descendente (Z-A)", "Fecha de lanzamiento"};
+    }
+
+    
+  }
+}
